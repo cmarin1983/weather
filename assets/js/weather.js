1ebc7306bcda18d19fd239b9de81dddaf4b0604e
Cliente CONTROL-PC <CltControl@GGIECLUGWRK161.espol.edu.ec>
2023-02-11 17:39:24 -0500

    otros cambios

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,7 +2,37 @@
 import { weather_data } from './data.js';
 
 let loadDayForecastData = () => {
-	
+	let { city_code, city, date, maxtemperature, mintemperature, cloudiness, wind, rainfall, ...others } = weather_data[0];
+
+    document.getElementById('city').innerText = city;
+    document.getElementById('date').innerText = date;
+    document.getElementById('maxtemperature').innerText = maxtemperature;
+    document.getElementById('mintemperature').innerText = mintemperature;
+    document.getElementById('cloudiness').innerText = cloudiness;
+    document.getElementById('wind').innerText = wind;
+    document.getElementById('rainfall').innerText = rainfall;
+
+    /*document.getElementById('late_icon').innerText = others.forecast_today[0].icon;
+    document.getElementById('late_temperature').innerText = others.forecast_today[0].temperature;
+    document.getElementById('late_tarde').innerText = others.forecast_today[0].tarde;
+    document.getElementById('late_forecast').innerText = others.forecast_today[0].forecast;
+
+    document.getElementById('late_icon').innerText = others.forecast_today[0].icon;
+    document.getElementById('late_temperature').innerText = others.forecast_today[0].temperature;
+    document.getElementById('late_tarde').innerText = others.forecast_today[0].tarde;
+    document.getElementById('late_forecast').innerText = others.forecast_today[0].forecast;
+*/
+    for(let elemet of others.forecast_today)
+    {
+        let forecast = elemet.lapse + '_forecast';
+        let icon = elemet.lapse + '_icon';
+        let text = elemet.lapse + '_text';
+        let temperature = elemet.lapse + '_temperature';
+        document.getElementById(forecast).innerText = elemet.forecast;
+        document.getElementById(icon).innerText = elemet.icon;
+        
+    }
+
 }
 
 let loadWeekForecastData = () => {
